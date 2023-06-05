4e0fc9cc519231cbdbd6c9351ca7e323796c1b62
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-06-05 11:01:42 -0400

    feat: add `Signin.jsx` component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {Signup} from './Signup';
+import {Signin} from './Signin';
 
 function App() {
   return (
     <div className="App">
       <h1>React App</h1>
       <Signup />
+      <Signin />
     </div>
   );
 }
