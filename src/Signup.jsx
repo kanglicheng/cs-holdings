49d03db3f92efa6e8fb95934b1a87fb000dd53e2
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-06-04 21:33:07 -0400

    feat: add auth + troubleshooting

---
+++ src/Signup.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createClient } from '@supabase/supabase-js'
+import { api_url } from './supaConstants';
+
+export const Signup = (props) => {
+
+
+    const supabase = createClient(api_url, process.env.REACT_APP_SUPABASE_ANON_KEY)
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const handleSignup = async (e) => {
+        e.preventDefault()
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+          })
+          if(error){
+            throw error;
+          }
+    }
+
+    return (
+
+        <div>
+            <form>
+                <h3>Create an account</h3>
+                <label>Email </label>
+                <input onChange={(e)=> setEmail(e.target.value)} type="text"/>
+
+                <label>Password </label>
+                <input onChange={(e)=>setPassword(e.target.value)} type="password"/>
+                <button onClick={handleSignup}>Sign up</button>
+            </form>
+        </div>
+
+    )
+
+}
